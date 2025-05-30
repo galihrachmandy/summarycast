98819b1e041e178f3978ccebba6ac5d301aec03f
galihrachmandy <85475490+galihrachmandy@users.noreply.github.com>
2025-05-30 07:42:47 +0700

    add index.js home page

---
+++ apps/summarycast/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>✅ SummaryCast is Alive!</h1>
+      <p>This is your Farcaster Mini App running on Next.js</p>
+    </div>
+  );
+}
